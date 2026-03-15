4a5f85c9722129c31eb1ebdb1aa193f7f036ea72
Ravie <aryaravie8@gmail.com>
2026-03-16 04:19:30 +0700

    second commit

---
+++ KodePos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL4_103022400147
+{
+    internal class KodePos
+    {
+        //array nyimpen nama kelurahan
+        string[] kelurahan =
+        {
+            "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari",
+            "Margasari", "Sekejati", "Kebonwaru", "Maleer"
+        };
+
+        //array nyimpen kode pos yang ber urutan sesuai kelurahan
+        int[] kodepos =
+        {
+            40266, 40287, 40267, 40256, 40287, 40286, 40286,
+            40286, 40272, 40274
+        };
+
+        //method nyari kode pos dari nama kelurahan
+        public int getKodePos(string inputKelurahan)
+        {
+            //perulangan untuk cek nama kelurahan 
+            for (int i = 0; i < kelurahan.Length; i++)
+            {
+                //jika nama kelurahan sama kayak inputan nama kelurahan 
+                if (kelurahan[i] == inputKelurahan)
+                {
+                    //ngembaliin kode pos yang urutanny sama
+                    return kodepos[i];
+                }
+            }
+            //klo inputannya gada return -1
+            return -1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MODUL4_103022400147
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KodePos kode = new KodePos();
+
+            //minta inputan nama kelurahan
+            Console.Write("Masukkan kelurahan: ");
+            string kel = Console.ReadLine();
+
+            //manggil method getKodePos buat nyari kode pos
+            int hasil = kode.getKodePos(kel);
+
+            // mengecek apakah kode pos ditemukan
+            if (hasil != -1)
+            {
+                Console.WriteLine("Kode Pos: " + hasil);
+            }
+            else
+            {
+                // jika tidak ditemukan
+                Console.WriteLine("Kelurahan tidak ditemukan");
+            }
+        }
+    }
+}
